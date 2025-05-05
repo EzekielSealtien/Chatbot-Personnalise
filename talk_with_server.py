1e1051d0c2e1d3b227864806f0700279b73d9f35
EzekielSealtien <EzekielTakam@teccart.online>
2025-05-05 03:21:50 -0400

    Update talk_with_server.py

--- talk_with_server.py
+++ talk_with_server.py
@@ -4,6 +4,7 @@ import requests
 BASE_URL = "https://calm-thicket-82645-76c53c6f1c73.herokuapp.com"
 #BASE_URL = "http://localhost:8010"
 
+
 def get_response_model(context,question):
     url=f"{BASE_URL}/responsechatbot"
     
