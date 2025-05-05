df12ee8aa673f6cf23597656d5e0de3ba8377de6
EzekielSealtien <EzekielTakam@teccart.online>
2025-05-05 03:13:11 -0400

    Update talk_with_server.py

--- talk_with_server.py
+++ talk_with_server.py
@@ -1,8 +1,8 @@
 import requests
 
 # Base URL for the FastAPI server
-#BASE_URL = "https://calm-thicket-82645-76c53c6f1c73.herokuapp.com"
-BASE_URL = "http://localhost:8010"
+BASE_URL = "https://calm-thicket-82645-76c53c6f1c73.herokuapp.com"
+#BASE_URL = "http://localhost:8010"
 
 def get_response_model(context,question):
     url=f"{BASE_URL}/responsechatbot"
